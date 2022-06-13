14390e3df79bfd908bf8c4db9118ff8124ab07c9
Makito Katsube <katsubemakito@gmail.com>
2022-06-13 23:42:24 +0900

    game.htmlのシナリオにコマンド制を導入し別ファイル化

---
+++ scenario.js
@@ -0,0 +1,13 @@
+scenario = [
+  ["SERIF",   "あなたの名前はなんですか？"],
+  ["SERIF",   "私の名前はポテト大王です。"],
+  ["SE",      "audio/se/se_enter.mp3"],
+  ["BGIMAGE", "url(image/back/fantasy_001.jpg)"],
+  ["CHARA",   "image/chara/9.png"],
+  ["SE-RAND", [
+                "audio/se/se_click.mp3",
+                "audio/se/se_enter.mp3",
+                "audio/se/se_turnpage.mp3"
+              ]
+  ]
+];
